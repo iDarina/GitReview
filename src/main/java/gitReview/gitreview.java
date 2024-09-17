c27d99511f019dea7235292b3ede06c826a02b43
iDaryna <dargud99@gmail.com>
2024-09-17 13:28:26 -0400

    added second line

--- src/main/java/gitReview/gitreview.java
+++ src/main/java/gitReview/gitreview.java
@@ -3,5 +3,7 @@ package gitReview;
 public class gitreview {
     public static void main(String[] args) {
         System.out.println("Hello main or master branch");
+
+        System.out.println("Hello from feature branch");
     }
 }
